cfbfa598ec7ee982059ee9bf56f86ddb0e9fc7a1
bl0ckp1n9 <blockp1n9@gmail.com>
2023-04-27 23:19:30 +0900

    feat: add custom spacing and width , height to  tailwind.config

--- src/components/ui/atoms/Example/Example.tsx
+++ src/components/ui/atoms/Example/Example.tsx
@@ -1,10 +1,10 @@
 import tw, { styled } from 'twin.macro'
 
-const Button = styled.button<{ color: string }>`
-  ${tw`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`}
+const Button = styled.button<{ color: string; className: string }>`
+  ${tw`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-100 rounded`}
   ${(props) =>
     props.color === 'primary' &&
-    tw`bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded`}
+    tw`bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-100 rounded`}
 `
 export function Example() {
   return <Button color="primary">Example</Button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+const px0_10 = { ...Array.from(Array(11)).map((_, i) => `${i}px`) }
+const px0_100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) }
+const px0_200 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) }
+const px0_300 = { ...Array.from(Array(301)).map((_, i) => `${i}px`) }
+const px0_400 = { ...Array.from(Array(401)).map((_, i) => `${i}px`) }
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -10,7 +16,10 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-      }
+      },
+      spacing: px0_200,
+      width: px0_400,
+      height: px0_400
     }
   },
   plugins: []
